22bc6be171ecf4fc0dd836ab493cd0123370e644
yantay0 <madinayantai2464@gmail.com>
2023-04-05 19:04:31 +0600

    register modeles

--- Lab8/shopback/api/admin.py
+++ Lab8/shopback/api/admin.py
@@ -11,4 +11,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price')
+    list_display = ('id', 'name', 'price', 'category')
